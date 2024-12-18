206530b7a9522d12b47a9421a4c5ca1086feaeb7
Eneskoy <koy.eness@gmail.com>
2024-12-18 17:34:55 +0300

    Update PriceAdjustmentTest.java
    price adjustment test base created

--- costcalculator-app/src/test/java/com/bera/yakup/hasan/enes/costcalculator/PriceAdjustmentTest.java
+++ costcalculator-app/src/test/java/com/bera/yakup/hasan/enes/costcalculator/PriceAdjustmentTest.java
@@ -1,4 +1,81 @@
 package com.bera.yakup.hasan.enes.costcalculator;
 
+import org.junit.*;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class PriceAdjustmentTest {
-}
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final InputStream originalIn = System.in;
+    private PriceAdjustment priceAdjustment;
+    private String ingredientTestFile = "ingredientTestFile.bin";
+
+    /**
+     * @throws Exception
+     * @brief This method is executed once before all test methods.
+     */
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    /**
+     * @throws Exception
+     * @brief This method is executed once after all test methods.
+     */
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+
+    /**
+     * @throws Exception
+     * @brief This method is executed before each test method.
+     */
+    @Before
+    public void setUp() throws Exception {
+        System.setOut(new PrintStream(outContent));
+        Scanner testScanner = new Scanner(System.in);
+        priceAdjustment = getPriceAdjustment(testScanner);
+    }
+
+    /**
+     * @throws Exception
+     * @brief This method is executed after each test method.
+     */
+    @After
+    public void tearDown() throws Exception {
+        System.setOut(originalOut);
+        System.setIn(originalIn);
+        deleteFile(ingredientTestFile);
+    }
+
+    private static PriceAdjustment getPriceAdjustment(Scanner testScanner) {
+        UserAuthentication userAuthentication = new UserAuthentication(testScanner, System.out);
+        IngredientManagement ingredientManagement = new IngredientManagement(userAuthentication, testScanner, System.out);
+        PriceAdjustment priceAdjustment = new PriceAdjustment(userAuthentication, ingredientManagement, testScanner, System.out);
+        return priceAdjustment;
+    }
+
+    private PriceAdjustment simulateUserInput(String input) {
+        ByteArrayInputStream inContent = new ByteArrayInputStream(input.getBytes());
+        Scanner testScanner = new Scanner(inContent);
+        return getPriceAdjustment(testScanner);
+    }
+
+    private void deleteFile(String filePath) throws IOException {
+        Files.deleteIfExists(Paths.get(filePath));
+    }
+
+    private void createIngredientFile() throws IOException {
+        try (DataOutputStream writer = new DataOutputStream(new FileOutputStream(ingredientTestFile))) {
+            for (int i = 1; i <= 5; i++) {
+                writer.writeInt(i);  // Ingredient ID
+                writer.writeUTF("Ingredient" + i);  // Ingredient Name
+                writer.writeFloat(15.0f * i);  // Ingredient Price
+            }
+        }
+    }
+
